54f89f58e9222c1b93afc1d7499911a918e11ce9
Sunil More <sunil9766@gmail.com>
2025-06-18 19:28:01 +0530

    crud

--- src/ai/llms.py
+++ src/ai/llms.py
@@ -2,18 +2,30 @@ from django.conf import settings
 from langchain_openai import ChatOpenAI
 
 
-def get_openai_api_key():
-    return settings.OPENAI_API_KEY
+AI_API_KEYS = {
+    "openai": settings.OPENAI_API_KEY,
+    "anthropic": settings.ANTHROPIC_API_KEY,
+    "google": settings.GOOGLE_API_KEY,
+    "azure": settings.AZURE_OPENAI_API_KEY,
+}
+
+AI_MODELS = {
+    "openai": "gpt-4o",
+    "anthropic": "claude-2",
+    "google": "gemini-2.0-flash",
+    "azure": "gpt-4",
+    "gpt-4-mini": "gpt-4-mini"
+}
 
 
-def get_openai_model(model="gpt-4o"):
+def get_openai_model(model=None):
     if model is None:
-        model = "gpt-4-mini"
+        model = AI_MODELS["openai"]
 
     return ChatOpenAI(
         model=model,
         temperature=0,
         max_tokens=None,
         max_retries=2,
-        api_key=get_openai_api_key()
+        api_key=AI_API_KEYS["openai"],
     )
--- src/ai/tools.py
+++ src/ai/tools.py
@@ -10,7 +10,8 @@ def list_documents(config: RunnableConfig):
     metadata = config.get('configurable') or config.get('metadata')
     user_id = metadata.get('user_id')
 
-    qs = Document.objects.filter(owner_id=user_id, active=True).order_by("-created_at")
+    qs = Document.objects.filter(
+        owner_id=user_id, active=True).order_by("-created_at")
     response_data = []
     for document in qs[:limit]:
         response_data.append({
@@ -23,22 +24,132 @@ def list_documents(config: RunnableConfig):
 @tool
 def get_document(config: RunnableConfig, document_id: int):
     """Get the details of a document by id"""
-    metadata = config.get('configurable') or config.get('metadata') 
+    metadata = config.get('configurable') or config.get('metadata')
     user_id = metadata.get('user_id')
     try:
-        document = Document.objects.get(id=document_id, owner_id=user_id, active=True)
+        document = Document.objects.get(
+            id=document_id, owner_id=user_id, active=True)
     except Document.DoesNotExist:
         return Exception("Document not found, try again")
     except Exception:
         return Exception("Invalid request for a document details not found, try again")
     response_data = {
-            "id": document.id,
-            "title": document.title,
-        }
+        "id": document.id,
+        "title": document.title,
+    }
+    return response_data
+
+
+@tool
+def create_document(config: RunnableConfig, title: str, content: str):
+    """ Create a new document for the current user
+
+    Args:
+        config (RunnableConfig): RunnableConfig object
+        title (str): document title
+        content (str): content of the document
+
+    Returns:
+        dict: document details
+    """
+    metadata = config.get('configurable') or config.get('metadata')
+    user_id = metadata.get('user_id')
+
+    if user_id is None:
+        raise Exception("Invalid request for user")
+
+    try:
+        document = Document.objects.create(
+            owner_id=user_id, title=title, content=content, active=True)
+    except Exception:
+        return Exception("Invalid request to create document, try again")
+
+    response_data = {
+        "id": document.id,
+        "title": document.title,
+        "content": document.content,
+        "created_at": document.created_at,
+        "updated_at": document.updated_at,
+        "active": document.active,
+    }
+    return response_data
+
+
+@tool
+def delete_document(config: RunnableConfig, document_id: int):
+    """Delete a document by id"""
+    metadata = config.get('configurable') or config.get('metadata')
+    user_id = metadata.get('user_id')
+
+    if user_id is None:
+        raise Exception("Invalid request for user")
+
+    try:
+        doc_obj = Document.objects.get(
+            id=document_id, owner_id=user_id, active=True)
+    except Document.DoesNotExist:
+        raise ("Document not found, try again")
+    except Exception:
+        raise Exception(
+            "Invalid request for a document details not found, try again")
+
+    doc_obj.delete()
+    response_data = {"message": "Document deleted successfully"}
+    return response_data
+
+
+@tool
+def update_document(config: RunnableConfig, document_id:int, title:str =None, content:str = None):
+    """ updae a document for the current user
+
+    Args:
+        config (RunnableConfig): RunnableConfig object
+        document_id (int): document id
+        title (str): document title
+        content (str): content of the document
+
+    Returns:
+        dict: document details
+    """
+    metadata = config.get('configurable') or config.get('metadata')
+    user_id = metadata.get('user_id')
+
+    if user_id is None:
+        raise Exception("Invalid request for user")
+
+    try:
+        doc_obj = Document.objects.get(
+            id=document_id, owner_id=user_id, active=True)
+    except Document.DoesNotExist:
+        raise ("Document not found, try again")
+    except Exception:
+        raise Exception(
+            "Invalid request for a document details not found, try again")
+
+    if title is not None:
+        doc_obj.title = title
+    if content is not None:
+        doc_obj.content = content
+
+    if title or content:
+        doc_obj.active = True
+        doc_obj.save()
+
+    response_data = {
+        "id": doc_obj.id,
+        "title": doc_obj.title,
+        "content": doc_obj.content,
+        "created_at": doc_obj.created_at,
+        "updated_at": doc_obj.updated_at,
+        "active": doc_obj.active,
+    }
     return response_data
 
 
 document_tools = [
     list_documents,
     get_document,
+    create_document,
+    delete_document,
+    update_document
 ]
