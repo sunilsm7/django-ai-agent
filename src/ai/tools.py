a8ddb1a3025dcd3438c676f178b68a198f94c4be
Sunil More <sunil9766@gmail.com>
2025-06-18 15:26:44 +0530

    doucment lookup

--- src/ai/tools.py
+++ src/ai/tools.py
@@ -1,11 +1,15 @@
+from langchain_core.runnables import RunnableConfig
 from langchain_core.tools import tool
 from documents.models import Document
 
 
 @tool
-def list_documents():
+def list_documents(config: RunnableConfig):
     """Get documents for the current user"""
-    qs = Document.objects.filter(active=True)
+    print(config)
+    metadata = config.get('metadata') or config.get('configurable')
+    user_id = metadata.get('user_id')
+    qs = Document.objects.filter(owner_id=user_id, active=True)
     response_data = []
     for document in qs:
         response_data.append({
@@ -16,9 +20,16 @@ def list_documents():
 
 
 @tool
-def get_document(document_id: int):
+def get_document(config: RunnableConfig, document_id: int):
     """Get the details of a document by id"""
-    document = Document.objects.get(id=document_id, active=True)
+    metadata = config.get('metadata') or config.get('configurable')
+    user_id = metadata.get('user_id')
+    try:
+        document = Document.objects.get(id=document_id, owner_id=user_id, active=True)
+    except Document.DoesNotExist:
+        return Exception("Document not found, try again")
+    except Exception:
+        return Exception("Invalid request for a document details not found, try again")
     response_data = {
             "id": document.id,
             "title": document.title,
