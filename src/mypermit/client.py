37c37802655754b0d89cd8a1589ff30df5cd9303
Sunil More <sunil9766@gmail.com>
2025-06-19 14:38:53 +0530

    permit

---
+++ src/mypermit/client.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from permit import Permit
+
+
+def get_permit_client():
+    pdp_url = settings.PERMIT_PDP_URL
+    api_key = settings.PERMIT_API_KEY
+    if not api_key:
+        raise ValueError("PERMIT_API_KEY was not set in django settings.")
+    return Permit(
+        pdp=pdp_url,
+        token=api_key
+    )
+
+
+permit_client = get_permit_client()
